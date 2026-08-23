42c2613604c5683ecf08a7f974bf23a3077e6369
382868331 <382868331@users.noreply.github.com>
2026-08-24 02:37:33 +0800

    test: reproduce PartialBlockGuard

---
+++ task_012_test.go
@@ -0,0 +1,3 @@
+package xxhash
+import "testing"
+func TestTaskXXHash012PartialBlockGuard(t *testing.T){got:=func() bool { d:=New(); d.Write([]byte("abc")); d.Write([]byte("defghijklmnopqrstuvwxyz0123456789")); return d.Sum64()==Sum64([]byte("abcdefghijklmnopqrstuvwxyz0123456789")) }();if got!=true{t.Fatalf("got %v want %v",got,true)}}
